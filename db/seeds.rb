b8102500e92ed1efecdaf2df9cfad1a2e437bddb
Guillaume Bonjean <gbonjean@free.fr>
2023-02-21 05:01:26 +0100

    Exo ok

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,14 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+
+puts 'creating fake restaurants...'
+10.times {
+  Restaurant.create(
+    name: Faker::Restaurant.name,
+    address: Faker::Address.full_address,
+    phone_number: Faker::PhoneNumber.phone_number,
+    category: ["chinese", "italian", "japanese", "french", "belgian"].sample
+  )
+}
+puts 'Ok !!'
